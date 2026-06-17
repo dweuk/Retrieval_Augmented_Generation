0c08608b60a66b684e608edd8b69ed93af5c1621
papot noe <papot.noe@gmail.com>
2026-06-18 01:09:47 +0200

    Parser and chuncker ok i think

--- test_ts.py
+++
@@ -1,7 +0,0 @@
-import tree_sitter_languages
-language = tree_sitter_languages.get_language("javascript")
-try:
-    q = language.query("(identifier) @id")
-except Exception as e:
-    import traceback
-    traceback.print_exc()
